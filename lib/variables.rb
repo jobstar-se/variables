cee86d2319258088b94f04e8a9fd47ba8319b24e
Peder Linder <pederbl@jobstar.se>
2010-11-17 17:06:37 +0000

    fixes

---
+++ lib/variables.rb
@@ -0,0 +1,31 @@
+module Variables
+
+end
+
+ActiveSupport.on_load(:active_record) do 
+  unless Object.const_defined? 'Variable'
+    klass = Class.new(ActiveRecord::Base) do
+      set_table_name 'variables'
+
+      def self.set(name, value)
+        update_all({ :name => name }, { :value => [value].to_json })
+      end
+    
+      def self.get(name)
+        ActiveSupport::JSON.decode(find_by_name(name)).first
+      end
+
+    end
+    Object.const_set 'Variable', klass
+  end
+
+  unless Variable.table_exists? 
+    ActiveRecord::Schema.define do 
+      create_table :variables do |t| 
+        t.string :name 
+        t.text   :value
+        t.timestamps
+      end
+    end 
+  end 
+end
---
+++ lib/variables/version.rb
@@ -0,0 +1,3 @@
+module Variables
+  VERSION = "0.0.1"
+end
